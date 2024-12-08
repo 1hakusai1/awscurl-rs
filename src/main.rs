257ab2c07a9ff33b98e407eb33cf28163de9ddd0
1hakusai1 <1hakusai1@gmail.com>
2024-12-08 15:13:39 +0900

    suppport other methods and request body

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use std::{env, time::SystemTime};
+use std::time::SystemTime;
 
 use aws_credential_types::provider::ProvideCredentials;
 use aws_sigv4::{
     http_request::{sign, SignableBody, SignableRequest, SigningSettings},
     sign::v4,
 };
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    url: String,
+
+    #[arg(short, long)]
+    data: Option<String>,
+
+    #[arg(short = 'X', long = "request")]
+    method: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    let url = &args[1];
+    let args = Args::parse();
+    let url = &args.url;
+    let method = args.method.unwrap_or("GET".to_string());
+    let body = args.data.unwrap_or("".to_string());
     let confg = aws_config::from_env().load().await;
     let identity = confg
         .credentials_provider()
@@ -28,20 +42,23 @@ async fn main() {
         .build()
         .unwrap()
         .into();
-    let signable_request =
-        SignableRequest::new("GET", url, std::iter::empty(), SignableBody::Bytes(&[])).unwrap();
+    let signable_request = SignableRequest::new(
+        &method,
+        url,
+        std::iter::empty(),
+        SignableBody::Bytes(body.as_bytes()),
+    )
+    .unwrap();
     let (instruction, _signature) = sign(signable_request, &signing_params)
         .unwrap()
         .into_parts();
-    let mut req = http::Request::new("");
-    instruction.apply_to_request_http1x(&mut req);
-    println!("{:#?}", req);
-    let res = reqwest::Client::new()
-        .get(url)
-        .headers(req.headers().clone())
-        .body(req.body().to_string())
-        .send()
-        .await
+    let mut http_req = http::Request::builder()
+        .method(method.as_bytes())
+        .uri(url)
+        .body(body)
         .unwrap();
-    println!("{:#?}", res);
+    instruction.apply_to_request_http1x(&mut http_req);
+    let reqwest_req: reqwest::Request = http_req.try_into().unwrap();
+    let res = reqwest::Client::new().execute(reqwest_req).await.unwrap();
+    println!("{}", res.text().await.unwrap());
 }
